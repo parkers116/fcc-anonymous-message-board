eaf0a998e6a5d59e330b35cb2967137592ae7b0e
Parker Suen <parkers116@gmail.com>
2020-04-24 19:46:00 +0800

    modify package.json & environment setting

--- server.js
+++ server.js
@@ -1,67 +1,81 @@
-'use strict';
+"use strict";
 
-var express     = require('express');
-var bodyParser  = require('body-parser');
-var expect      = require('chai').expect;
-var cors        = require('cors');
+const mongoose = require("mongoose");
+const helmet = require("helmet");
+var express = require("express");
+var bodyParser = require("body-parser");
+var expect = require("chai").expect;
+var cors = require("cors");
 
-var apiRoutes         = require('./routes/api.js');
-var fccTestingRoutes  = require('./routes/fcctesting.js');
-var runner            = require('./test-runner');
+var apiRoutes = require("./routes/api.js");
+var fccTestingRoutes = require("./routes/fcctesting.js");
+var runner = require("./test-runner");
+
+require("dotenv").config();
 
 var app = express();
 
-app.use('/public', express.static(process.cwd() + '/public'));
+mongoose.connect(process.env.MONGODB_URI, {
+  dbName: "fcc-anonymous-message-board",
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+mongoose.connection.on(
+  "error",
+  console.error.bind(console, "MongoDB connection error:")
+);
+mongoose.connection.once("open", () => {
+  console.log("DB connection successful");
+});
 
-app.use(cors({origin: '*'})); //For FCC testing purposes only
+app.use(helmet());
+// app.use(helmet().frameguard());
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use("/public", express.static(process.cwd() + "/public"));
+
+app.use(cors({ origin: "*" })); //For FCC testing purposes only
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 //Sample front-end
-app.route('/b/:board/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/board.html');
-  });
-app.route('/b/:board/:threadid')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/thread.html');
-  });
+app.route("/b/:board/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/board.html");
+});
+app.route("/b/:board/:threadid").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/thread.html");
+});
 
 //Index page (static HTML)
-app.route('/')
-  .get(function (req, res) {
-    res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.route("/").get(function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
+});
 
 //For FCC testing purposes
 fccTestingRoutes(app);
 
-//Routing for API 
+//Routing for API
 apiRoutes(app);
 
 //Sample Front-end
 
-    
 //404 Not Found Middleware
-app.use(function(req, res, next) {
-  res.status(404)
-    .type('text')
-    .send('Not Found');
+app.use(function (req, res, next) {
+  res.status(404).type("text").send("Not Found");
 });
 
 //Start our server and tests!
 app.listen(process.env.PORT || 3000, function () {
   console.log("Listening on port " + process.env.PORT);
-  if(process.env.NODE_ENV==='test') {
-    console.log('Running Tests...');
+  if (process.env.NODE_ENV === "test") {
+    console.log("Running Tests...");
     setTimeout(function () {
       try {
         runner.run();
-      } catch(e) {
+      } catch (e) {
         var error = e;
-          console.log('Tests are not valid:');
-          console.log(error);
+        console.log("Tests are not valid:");
+        console.log(error);
       }
     }, 1500);
   }
